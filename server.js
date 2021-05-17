2f19ff25c9f89aa0bcb3a5b51ec74bc5a7a62f9d
kylechung10 <47342503+kylechung10@users.noreply.github.com>
2021-05-17 18:27:39 -0400

    Path bug testing

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ App.patch("/api/games/:pin", async (req, res) => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  App.use(Express.static("client/build"));
+  App.use(Express.static(Path.join(__dirname, "client", "build")));
   App.get("*", (req, res) => {
     res.sendFile(Path.join(__dirname, "client", "build", "index.html"));
   });
